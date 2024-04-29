6fd3c5ccd7c7fce6b94173e301a4425e0085d774
JennyZhu2 <jenz1236@gmail.com>
2024-04-28 21:33:41 -0400

    fixed view display and added form for data inputs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
@@ -27,11 +26,22 @@ connection.commit()
 # for x in cursor:
 #     print(x)
 
-@app.route('/customer')
+@app.route('/customer', methods=['POST', 'GET'])
 def customer():
-    # cursor = connection.cursor()
-    cursor.execute("SELECT * FROM users")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM customers")
     customer = cursor.fetchall()
+    if request.method == "POST":
+        uname = request.form['uname']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        address = request.form['address']
+        bdate = request.form['bdate']
+        rating = request.form['rating']
+        credit = request.form['credit']
+        cursor.callproc('add_customer', (uname, fname, lname, address, bdate, rating, credit))
+        connection.commit()
+    cursor.close()
     return render_template('customer.html', customer=customer)
 
 @app.route('/view')
@@ -40,47 +50,4 @@ def view():
 
 if __name__ == '__main__':
     app.run()
-=======
-from flask import Flask, render_template
-import mysql.connector
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'akfbibibibfkwiw'
-
-# Establishes local connection with database
-connection = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    passwd="1236",
-    database="drone_dispatch",
-    port = 3306
-)
-
-# cursor object interacts with database
-cursor = connection.cursor()
-cursor = connection.cursor(buffered=True)
-#cursor.execute("INSERT INTO users (uname, first_name, last_name, address, birthdate) VALUES (%s,%s,%s,%s,%s)", ("Mary143", "Mary", "Joe", "San Diego", "1973-03-07"))
-connection.commit()
-
-# cursor.callproc('add_customer', ("Jenn482","Jenny","ZHU","Atlanta","2004-07-10","5","10"))
-# cursor.execute("DESCRIBE users")
-# cursor.execute("SELECT * FROM users")
-
-# for x in cursor:
-#     print(x)
-
-@app.route('/customer')
-def customer():
-    # cursor = connection.cursor()
-    cursor.execute("SELECT * FROM users")
-    customer = cursor.fetchall()
-    return render_template('customer.html', customer=customer)
-
-@app.route('/view')
-def view():
-    return 'Views will be displayed here'
-
-if __name__ == '__main__':
-    app.run()
->>>>>>> 14e08ebf143df5115264c7730533cf806a0ec1fd
     
